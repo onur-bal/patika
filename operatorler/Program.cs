d5ce07cc8f46557c71d0966df7f15faa11348627
onur bal <onurball@hotmail.com>
2021-10-19 22:08:50 +0300

    operatorler

---
+++ operatorler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 5;
+            System.Console.WriteLine(x+y);
+        }
+    }
+}
