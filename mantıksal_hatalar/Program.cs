3e25a821c990e0f2bafe0b1e7ce8ae19b8db5deb
onur bal <onurball@hotmail.com>
2021-10-21 15:48:35 +0300

    hata_ayıklama

---
+++ mantıksal_hatalar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace mantıksal_hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+              Console.WriteLine("bir sayı giriniz");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              System.Console.WriteLine("girdiniğiniz sayı= "+ sayi);
+           }
+           catch(Exception ex)
+           {
+                System.Console.WriteLine( "hata: " + ex.Message.ToString());
+           }
+           finally
+           {
+               System.Console.WriteLine("işlem tamamlandı");
+           }
+           
+            
+        }
+    }
+}
