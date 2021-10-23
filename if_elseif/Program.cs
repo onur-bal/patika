0348ff49f6aa1ed19a9702094ed41abc15644d32
onur bal <onurball@hotmail.com>
2021-10-23 22:14:29 +0300

    if-elseif

---
+++ if_elseif/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time > 18)
+            Console.WriteLine("iyi akşamlar");
+            else if (time >= 6 && time<=11)
+            System.Console.WriteLine("günaydın");
+            else
+            System.Console.WriteLine("iyi akşamlar");
+
+        }
+    }
+}
