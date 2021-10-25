2ecf25700fad48dd877165035810162c621f6f2d
onur bal <onurball@hotmail.com>
2021-10-25 21:56:11 +0300

    donguler_while_foreach

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz");
+            int sayi=int.Parse(Console.ReadLine());
+            int top = 0;
+            int sayac=1;
+            while (sayac<=sayi)
+            {
+                top+=sayac;
+                sayac ++ ;
+            }
+                 System.Console.WriteLine("ortalama=" + top/sayi);
+
+                 string[] arabalar ={"nissan","dacia","ford","bmw"};
+
+                 foreach (var araba in arabalar)
+                 {
+                     System.Console.WriteLine(araba);
+                 }
+        }
+    }
+}
