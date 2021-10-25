c251b7f87265a05a00ad6787d9111cb8107cbcdc
onur bal <onurball@hotmail.com>
2021-10-25 20:10:27 +0300

    loop

---
+++ donguler_loop_for/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace donguler_loop_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz!");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i < sayac; i++)
+            {
+                if (i%2==1)
+                System.Console.WriteLine(i);
+            }
+
+           int tektoplam=0;
+          int  cifttoplam=0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i%2==1)
+                tektoplam+=i;
+                else
+                    cifttoplam+=i;
+                
+            } 
+                System.Console.WriteLine("cift toplamları = " + cifttoplam );
+                System.Console.WriteLine("tek toplamları = " + tektoplam );
+
+                
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
