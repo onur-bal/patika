1c04807c9e2bffb750e08e502ec1073068202c4a
onur bal <onurball@hotmail.com>
2021-10-30 15:05:51 +0300

    generic

---
+++ generic_list/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>SayiListesi=new List <int>(); 
+            
+            SayiListesi.Add(3);
+            SayiListesi.Add(33);
+            SayiListesi.Add(39);
+            SayiListesi.Add(38);
+            SayiListesi.Add(31);
+            SayiListesi.Add(323);
+           System.Console.WriteLine( SayiListesi.Count);
+           foreach (var sayi in SayiListesi)
+           {
+               System.Console.WriteLine(sayi);
+           }
+
+
+            
+            
+            
+            
+        }
+    }
+}
