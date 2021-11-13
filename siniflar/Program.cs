a1634f816c47a26b56faed5683104fc1145cdc4c
onur bal <onurball@hotmail.com>
2021-11-13 17:58:17 +0300

    siniflar

---
+++ siniflar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace siniflar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calısan1=new Calisan();
+            calısan1.Ad="onur";
+            calısan1.SoyAd="BAL";
+            calısan1.No=22222222;
+            calısan1.Departman="IT";
+            calısan1.CalisanBilgileri();
+
+        }
+
+        class Calisan
+        {
+            public string Ad;
+            public string SoyAd;
+            public int No;
+            public string Departman;
+
+            public void CalisanBilgileri()
+            {
+                System.Console.WriteLine("Çalışan Adı:{0}",Ad);
+                System.Console.WriteLine("Çalışan SoyaAdı:{0}",SoyAd);
+                System.Console.WriteLine("Çalışan No:{0}",No);
+                System.Console.WriteLine("Çalışan Departmanı:{0}",Departman);
+
+            }
+            
+
+        }
+    }
+}
